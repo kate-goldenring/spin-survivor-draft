8c059c5f697af2c2700979d0ae07ec2ab76247b1
Kate Goldenring <kate.goldenring@fermyon.com>
2024-09-19 11:02:54 -0700

    Automate deletion of drafterDrafts on drafter deletion
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- migration.up.sql
+++ migration.up.sql
@@ -1,18 +1,20 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS players (
-  name TEXT PRIMARY KEY,
+  name TEXT NOT NULL PRIMARY KEY,
   season INTEGER,
   voted_out DATE
 );
 
 CREATE TABLE IF NOT EXISTS drafters (
-  name TEXT PRIMARY KEY,
+  name TEXT NOT NULL PRIMARY KEY,
   season INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS drafterDrafts (
   drafter_id INTEGER,
   player_id INTEGER,
-  FOREIGN KEY (drafter_id) REFERENCES drafters(name),
-  FOREIGN KEY (player_id) REFERENCES players(name),
+  FOREIGN KEY (drafter_id) REFERENCES drafters(name) ON DELETE CASCADE,
+  FOREIGN KEY (player_id) REFERENCES players(name)  ON DELETE CASCADE,
   PRIMARY KEY (drafter_id, player_id)
 );
